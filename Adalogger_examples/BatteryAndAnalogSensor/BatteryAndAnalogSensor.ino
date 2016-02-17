82b9ce9bb51b8227af5f3e4406d319eadebc7d9e
tigoe <tom.igoe@gmail.com>
2016-02-16 19:50:14 -0500

    Updated Battery and analog sensor example

--- Adalogger_examples/BatteryAndAnalogSensor/BatteryAndAnalogSensor.ino
+++ Adalogger_examples/BatteryAndAnalogSensor/BatteryAndAnalogSensor.ino
@@ -3,9 +3,10 @@
   This sketch reads the battery level and analog input A0
   on an Adalogger and saves their values to a comma-separated values (CSV) file.
 
-  Written and tested on an Adalogger 32U4 board
+  Written and tested on an Adalogger M0 board
   .
   created 22 Jan 2016
+  modified 16 Feb 2016
   by Tom Igoe
 
 */
@@ -13,8 +14,9 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int batteryPin = A9;        // battery volotage is on pin A9
+const int batteryPin = A7;        // battery volotage is on pin A7
 const int chipSelect = 4;         // SPI chip select for SD card
+const int cardDetect = 7;          // pin that detects whether the card is there
 const int writeLed = 8;           // LED indicator for writing to card
 const int errorLed = 13;          // LED indicator for error
 long lastWriteTime = 0;           // timestamp for last write attempt
@@ -22,69 +24,89 @@ long interval = 10000;            // time between readings
 char fileName[] = "datalog.csv";  // filename to save on SD card
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);   // initialize serial communication
 
-  // initialize LED pins:
+  // initialize LED and cardDetect pins:
   pinMode(writeLed, OUTPUT);
   pinMode(errorLed, OUTPUT);
+  pinMode(cardDetect, INPUT_PULLUP);
 
-  // check to see that the SD card is responding:
-  if (!SD.begin(chipSelect)) {      // if not,
-    digitalWrite(errorLed, HIGH);   // turn on error LED
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (true);
+  // Stay in this loop until the card is inserted:
+  while (digitalRead(cardDetect) == LOW) {
+    Serial.println("Waiting for card...");
+    digitalWrite(errorLed, HIGH);
+    delay(750);
   }
-  // if the card initialized successfully
-  Serial.println("card initialized.");
 
-  // add header columns to file:
-  saveToFile("Battery Voltage,Sensor Reading");
+  // check if the card initialized successfully:
+  if (startSDCard() == true) {
+    Serial.println("card initialized.");
+    delay(100);
+  } else {
+    Serial.println("Card failed");
+  }
+  // open the log file:
+  File logFile = SD.open(fileName, FILE_WRITE);
+  // write header columns to file:
+  if (logFile) {
+    logFile.println("Battery Voltage,Sensor Reading");
+    logFile.close();
+  }
 }
 
 void loop() {
+  // if the cart's not there, don't do anything more:
+  if (digitalRead(cardDetect) == LOW) {
+    digitalWrite(errorLed, HIGH);
+    return;
+  }
+  // turn of the error LED:
+  digitalWrite(errorLed, LOW);
+
   // read sensors every 10 seconds
   if (millis()  - lastWriteTime >=  interval) {
-    // read battery voltage:
-    float batteryVoltage = analogRead(batteryPin);
-    // voltage is halved by the on-board voltage divider,
-    // so real voltage is * 2:
-    batteryVoltage =  batteryVoltage * 2;
-    // multiply by reference voltage (3.3) and divide by analog resolution:
-    batteryVoltage = (batteryVoltage * 3.3) / 1024;
+    File logFile = SD.open(fileName, FILE_WRITE);   // open the log file
+    if (logFile) {                                  // if you can write to the log file,
+      digitalWrite(writeLed, HIGH);                 // turn on the write LED
+      float batteryVoltage = readBattery();         // read the battery voltage
 
-    // sensor input pin is 0 - 3.3V. Read and convert:
-    float sensorVoltage = analogRead(A0);
-    sensorVoltage = (sensorVoltage * 3.3) / 1024;
+      // sensor input pin is 0 - 3.3V. Read and convert:
+      float sensorVoltage = analogRead(A0);
+      sensorVoltage = (sensorVoltage * 3.3) / 1024;
 
-    // make a string to print to the data file and save it:
-    String reading = String(batteryVoltage);
-    reading += ",";
-    reading += sensorVoltage;
-    saveToFile(reading);
+      logFile.print(batteryVoltage);      // print battery voltage to the log
+      logFile.print(",");                 // print comma to the log
+      logFile.println(sensorVoltage);     // print sensor voltage & newline to the log
+      logFile.close();                    // close the file
+      
+      // for degugging only:
+      Serial.print(batteryVoltage);
+      Serial.print(",");
+      Serial.println(sensorVoltage);
 
-    // update the last attempted save time:
-    lastWriteTime = millis();
+      // update the last attempted save time:
+      lastWriteTime = millis();
+    }
+    digitalWrite(writeLed, LOW);      // turn off the write LED
   }
 }
 
-boolean saveToFile(String dataString) {
-  boolean result = false;
-  // open the file:
-  File dataFile = SD.open(fileName, FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    digitalWrite(errorLed, LOW);    //all is good, make sure error LED is off
-    digitalWrite(writeLed, HIGH);   // turn on write LED
-    dataFile.println(dataString);   // write data to file
-    dataFile.close();               // close file
-    digitalWrite(writeLed, LOW);    // turn off write LED
-    result = true;                  // you made a successful write
-  } else {
-    digitalWrite(errorLed, HIGH);   // couldn't write, turn on error LED
-    result = false;                 // you failed to write
+boolean startSDCard() {
+  // check to see that the SD card is responding:
+  if (!SD.begin(chipSelect)) {      // if not,
+    digitalWrite(errorLed, HIGH);   // turn on error LED
+    return false;
   }
-  return result;
+  return true;
 }
 
+float readBattery() {
+  // read battery voltage:
+  float batteryV = analogRead(batteryPin);
+  // voltage is halved by the on-board voltage divider,
+  // so real voltage is * 2:
+  batteryV =  batteryV * 2;
+  // multiply by reference voltage (3.3) and divide by analog resolution:
+  batteryV = (batteryV * 3.3) / 1024;
+  return batteryV;
+}

a03ccc2fd2b9258eca27c8bf735a13d317f51119
Tom Igoe <tom.igoe@gmail.com>
2021-06-09 12:01:56 -0400

    Update arduino_secrets.h

--- WiFiDatalogger/DataLoggerHttpClientJSON_MAC/arduino_secrets.h
+++ WiFiDatalogger/DataLoggerHttpClientJSON_MAC/arduino_secrets.h
@@ -1,4 +1,3 @@
 #define SECRET_SSID ""    //  your network SSID (name)
 #define SECRET_PASS ""    // your network password 
-#define SECRET_SERVER "tigoe-datalogger.glitch.me"  // your server address
-#define LOGGER_VERSION 0003
+#define SECRET_SERVER ""  // your server address

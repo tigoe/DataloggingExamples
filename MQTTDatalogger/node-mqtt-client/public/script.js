726495144d4a9e11676db579919ccdbe8c125a63
Tom Igoe <tom.igoe@gmail.com>
2021-06-14 08:15:26 -0400

    Update script.js

--- MQTTDatalogger/node-mqtt-client/public/script.js
+++ MQTTDatalogger/node-mqtt-client/public/script.js
@@ -1,8 +1,7 @@
 /*
-  Hue Hub light control over MQTT
+  MQTT Client
   
-  created 23 July 2020
-  modified 23 Nov 2020
+  created 13 Jun 2021
   by Tom Igoe
 */
 
@@ -16,16 +15,15 @@ let broker = {
 let client;
 // client credentials:
 let creds = {
-   clientID: 'myClient',
+   clientID: 'htmlClient',
    userName: 'public',
    password: 'public'
 }
 // topic to subscribe to when you connect to the broker:
-let topic = 'lights';
-
+let topic = 'light-readings';
+let localDiv, remoteDiv;
 
 function setup() {
-
    // Create an MQTT client:
    client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
    // set callback handlers for the client:
@@ -33,31 +31,59 @@ function setup() {
    client.onMessageArrived = onMessageArrived;
    // connect to the MQTT broker:
    client.connect(
-       {
-           onSuccess: onConnect,       // callback function for when you connect
-           userName: creds.userName,   // username
-           password: creds.password,   // password
-           useSSL: true                // use SSL
-       }
+      {
+         onSuccess: onConnect,       // callback function for when you connect
+         userName: creds.userName,   // username
+         password: creds.password,   // password
+         useSSL: true                // use SSL
+      }
    );
+   // add handles for the divs:
+   localDiv = document.getElementById('local');
+   remoteDiv = document.getElementById('remote');
 }
 
 
 // called when the client connects
 function onConnect() {
-   localDiv.html('client is connected');
+   localDiv.innerHTML = 'client is connected to '
+      + broker.hostname + '<br>and subscribed to '
+      + topic;
    client.subscribe(topic);
 }
 
 // called when the client loses its connection
 function onConnectionLost(response) {
    if (response.errorCode !== 0) {
-       localDiv.html('onConnectionLost:' + response.errorMessage);
+      localDiv.innerHTML = 'Connection Lost:' + response.errorMessage;
    }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-   remoteDiv.html('I got a message:' + message.payloadString);
-   let  incomingNumber = parseInt(message.payloadString);
+   // get the payload string and make it a JSON object:
+   let msg = JSON.parse(message.payloadString);
+// iterate over the elements of the JSON object:
+   for (var key in msg) {
+      // If there is not an HTML element with the same ID:
+      if (document.getElementById(key) === null) {
+         // create create one and give it this ID:
+         let thisDiv = document.createElement('div');
+         thisDiv.id = key;
+         // create text with the key and value:
+         let textNode = document.createTextNode(key + ': ' + msg[key]);
+         // add the text to the element and add it to the HTML:
+         thisDiv.append(textNode);
+         document.body.append(thisDiv);
+      } else {
+         // if there's already an element with this ID,
+         // just update it:
+         let thisDiv = document.getElementById(key);
+         thisDiv.innerHTML = key + ': ' + msg[key];
+      }
+   }
 }
+
+// This is a listener for the page to load.
+// This is the command that actually starts the script:
+window.addEventListener('DOMContentLoaded', setup);

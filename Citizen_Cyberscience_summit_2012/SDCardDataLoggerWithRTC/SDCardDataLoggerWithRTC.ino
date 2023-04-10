d039e4115d09fbccb808fabc27fe3ae56d470fd9
Tom Igoe <tom.igoe@gmail.com>
2023-04-10 09:31:10 -0400

    Update SDCardDataLoggerWithRTC.ino

--- Citizen_Cyberscience_summit_2012/SDCardDataLoggerWithRTC/SDCardDataLoggerWithRTC.ino
+++ Citizen_Cyberscience_summit_2012/SDCardDataLoggerWithRTC/SDCardDataLoggerWithRTC.ino
@@ -4,19 +4,17 @@
  This example shows how to log data from 
  a BMP085 temperature and pressure sensor 
  to an SD card using the SD library. It incorporates
- a DS1307/DS3231 Realtime clock
+ a DS3231 Realtime clock
  
- It also uses the Adafruit DHT sensor library from 
- https://github.com/adafruit/DHT-sensor-library
- and the Adafruit Realtime Clock library:
- https://github.com/adafruit/RTClib
- And the BMP085 library:
- https://github.com/adafruit/BMP085-Library
+ It  uses following libraries:
+  http://librarymanager/All#RTClib
+  http://librarymanager/All#BMP085-library
+  http://librarymanager/All#SD
  
  The circuit:
  * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
+ ** SDI - pin 11
+ ** SDO - pin 12
  ** CLK - pin 13
  ** CS - pin 4
  * BMP085 sensor connected as follows:
@@ -31,57 +29,59 @@
  ** SDA to A5
  
  created  20 Feb 2012
+ updated 10 Apr 2023
  by Tom Igoe
  
  This example code is in the public domain, subject to the licenses
  of the libraries used.
- 
  */
 
 #include <SD.h>
-#include <BMP085.h>
+#include <Adafruit_BMP085.h>
 #include <Wire.h>
 #include "RTClib.h"
 
-BMP085 bmp;              // library for the sensor
-RTC_DS1307 RTC;          // library for the Realtime Clock
+Adafruit_BMP085 bmp;  // library for the sensor
+RTC_DS3231 rtc;       // library for the Realtime Clock
 
-const int interval = 10; // the interval between reads, in seconds
-int lastReadTime = 0;    // the last time you read the sensor, in seconds
+const int interval = 10;  // the interval between reads, in seconds
+int lastReadTime = 0;     // the last time you read the sensor, in seconds
 
 
 // On the Ethernet Shield, CS is pin 4. Note that even if it's not
 // used as the CS pin, the hardware CS pin (10 on most Arduino boards,
 // 53 on the Mega) must be left as an output or the SD library
 // functions will not work.
-const int chipSelect = 4; 
-char fileName[] = "datalog.csv";    // filename to save to
+const int chipSelect = 4;
+char fileName[] = "datalog.csv";  // filename to save to
 
 void setup() {
   // start serial:
   Serial.begin(9600);
   // start the realtime clock:
   Wire.begin();
-  RTC.begin();
-  
-  // if the clock isn't running, set the time:
-  if (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    RTC.adjust(DateTime(__DATE__, __TIME__));
-  }
+  rtc.begin();
+
+  // following line sets the RTC to the date & time this sketch was compiled
+  rtc.adjust(DateTime(__DATE__, __TIME__));
+
   // start the sensor and initialize the SD card:
   bmp.begin();
   startSDCard();
 }
 
-void loop()
-{
-  // Get the current time from the RTC:
-  DateTime now = RTC.now();
+void loop() {
+  // Get the current time from the rtc:
+  DateTime now = rtc.now();
 
-// if ten seconds have passed:
-  if (abs(now.second() - lastReadTime) >= interval) {
+  int change = now.second() - lastReadTime;
+  // if the change is negative, combine the remainder of
+  // the last minute and what's passed of this minute:
+  if (change < 0) {
+    change = (60 - lastReadTime) + now.second();
+  }
+  // see if the interval has passed:
+  if (change >= interval) {
     float temperature = bmp.readTemperature();
     float pressure = bmp.readPressure();
 
@@ -98,7 +98,7 @@ void loop()
       dataFile.print("\t");
       dataFile.println(temperature);
       // print to the serial port too:
-      Serial.print(timeStamp);
+      Serial.println(timeStamp);
       Serial.print("\t");
       Serial.print(pressure);
       Serial.print("\t");
@@ -107,11 +107,11 @@ void loop()
       lastReadTime = now.second();
       // close the datafile:
       dataFile.close();
-    }  
+    }
     // if the file isn't open, pop up an error:
     else {
       Serial.println("error opening datalog.csv");
-    } 
+    }
   }
 }
 
@@ -121,14 +121,14 @@ boolean startSDCard() {
   Serial.print("Initializing SD card...");
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
-  pinMode(10, OUTPUT);
+  pinMode(chipSelect, OUTPUT);
 
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
     result = false;
-  } 
+  }
   else {
     Serial.println("card initialized.");
     File dataFile = SD.open(fileName, FILE_WRITE);
@@ -137,7 +137,7 @@ boolean startSDCard() {
       dataFile.println("Date/Time\tPressure(Pa)\tTemperature");
       dataFile.close();
     }
-  }  
+  }
   return result;
 }
 
@@ -145,20 +145,15 @@ boolean startSDCard() {
 String readTimestamp(DateTime thisTime) {
   // concatenate a string with all the parts of the date and time:
   String result = String(thisTime.day());
-  result +="/";
-  result +=String(thisTime.month());
-  result +="/";
-  result +=String(thisTime.year());
-  result +=" ";
-  result +=String(thisTime.hour());
-  result +=":";
-  result +=String(thisTime.minute());
-  result +=":";
-  result +=String(thisTime.second());
-  return result; 
+  result += "/";
+  result += String(thisTime.month());
+  result += "/";
+  result += String(thisTime.year());
+  result += " ";
+  result += String(thisTime.hour());
+  result += ":";
+  result += String(thisTime.minute());
+  result += ":";
+  result += String(thisTime.second());
+  return result;
 }
-
-
-
-
-
